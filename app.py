789aaa4b9c06d5d321dfa5b82032db69a830b823
diegoaaf100-sketch <diegoaaf.100@gmail.com>
2026-09-23 11:59:33 -0300

    Add additional image to app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.title("📊 DGP - Dados dos Militares")
 # --- ADICIONAR IMAGEM NO TOPO ---
 # Substitua "logo.png" pelo nome exato do arquivo que você enviou ao GitHub
 st.image("images.png", width=250)
+st.image("11679.png", width=250)
 
 # Função para carregar os dados do Google Sheets
 @st.cache_data(ttl=5)
